e287bc5e11bd4fbcf83e752f67e8a3bf001a1a82
Zyan <coderZyan@outlook.com>
2020-07-12 17:50:03 +0800

    sum obj

--- 01sum.js
+++ 01sum.js
@@ -1,10 +1,35 @@
+/**
+ * Map
+ */
 let sum = (arr, target) => {
-    let map = new Map()
-    for(let i = 0; i++; i< arr.length){
-        let diff = target - arr[i]
-        if(map.has(diff)) {
-            return [map.get(diff),i]
-        }
-        map.set(arr[i],i);
+  let map = new Map()
+  for (let i = 0; i < arr.length; i++) {
+    let diff = target - arr[i]
+    if (map.has(diff)) {
+      return [map.get(diff), i]
     }
+    map.set(arr[i], i)
+  }
 }
+
+/**
+ * Obj
+ */
+
+let sum01 = (arr, total) => {
+  let obj = {}
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i]
+    let diff = total - item
+    if (obj[diff]) {
+      return [obj[diff], i]
+    }
+    obj[item] = i
+  }
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7]
+let sumTotal = 13
+
+console.log(sum(arr, sumTotal))
+console.log(sum01(arr, sumTotal))
